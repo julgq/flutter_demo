f118d3f4b70b12e9eafda031de0d08a131f37d60
Julio <jc.gutierrezq@gmail.com>
2019-01-28 17:04:50 -0600

    MeterialApp function

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@
 import 'package:flutter/material.dart';
 
 // Define a 'main' function to run when our app starts
-
-// Create a new text widget to show some text
+void main() {
+  // Create a new text widget to show some text
 // on the screen
+  var app = MaterialApp(home: Text('Hi there!'));
 
 // Take that widget and get it on the screen
-
-
+}
