9b0aceef3f5f987c7398ccdcf81787eb72439aaa
Julio <jc.gutierrezq@gmail.com>
2019-01-29 18:58:47 -0600

    setState()

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,29 +7,28 @@ class App extends StatefulWidget {
     // TODO: implement createState
     return _App();
   }
-
 }
 
 class _App extends State<App> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
         home: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              print('Hi there!');
-            },
-          ),
-          appBar: AppBar(
-            title: Text('Lets see some images'),
-          ),
-        )
-
-    );
+      body: Text('$counter'),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+          print(counter);
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images'),
+      ),
+    ));
   }
-
 }
-
-
